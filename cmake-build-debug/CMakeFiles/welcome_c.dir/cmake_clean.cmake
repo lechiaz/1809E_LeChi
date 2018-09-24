4bc945b69bfb474848db9d676d3c7583294526e7
unknown <you@example.com>
2018-09-24 18:36:07 +0700

    Hello_C

---
+++ cmake-build-debug/CMakeFiles/welcome_c.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/welcome_c.dir/main.c.obj"
+  "welcome_c.pdb"
+  "welcome_c.exe"
+  "welcome_c.exe.manifest"
+  "welcome_c.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/welcome_c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/welcome_c.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for welcome_c.
+# This may be replaced when dependencies are built.
